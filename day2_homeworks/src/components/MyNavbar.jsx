62e3c869829cc1a200be5433291f52599971f371
klevinb <klevin.bazaiti98@gmail.com>
2020-06-03 19:21:20 +0200

    page logo upload!

--- day2_homeworks/src/components/MyNavbar.jsx
+++ day2_homeworks/src/components/MyNavbar.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
+import logo from '../assets/navbar_logo.png'
 
 class NavBar extends Component {
     render(){
         return(
             <>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">BIG BOOK</Navbar.Brand>
+                <Navbar.Brand href="#home"><img src={logo} width="120px" /></Navbar.Brand>
                 <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#about">About</Nav.Link>
