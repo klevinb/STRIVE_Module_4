6ff86192be93e26aa6cf2bad75bc5caa3d44e3c9
klevinb <klevin.bazaiti98@gmail.com>
2020-06-04 13:41:20 +0200

    Day3 Pair Programming

---
+++ day3_pairprogramming/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AlertMessage from './components/WarningSign'
+import Badge from './components/MyBadge'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
