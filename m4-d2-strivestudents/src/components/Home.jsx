79c93ba23ca29ecde6ab6ddc4e35ab7a5918fe59
klevinb <klevin.bazaiti98@gmail.com>
2020-06-04 12:15:44 +0200

    DishComments component was created

--- m4-d2-strivestudents/src/components/Home.jsx
+++ m4-d2-strivestudents/src/components/Home.jsx
@@ -26,32 +26,34 @@ class Home extends Component {
         console.log(this.props)
         return (
             <>
-            <Container>
-                <Row className="justify-content-center mt-3">
-                    <Col>
-                        <h1>Welcome to Strivesturant !</h1>
-                        <p className="lead">The best page for all gratuated students!</p>
-                        <hr className=""></hr>
-                        <Carousel>
-                            {items.map((item) => {
-                                return (
-                                    <Carousel.Item key={item.id}>
-                                    <img
-                                    className="d-block w-100"
-                                    src={item.image}
-                                    alt={item.name}
-                                    />
-                                    <Carousel.Caption>
-                                    <h3>{item.name}</h3>
-                                    <p>{item.description}</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                                )
-                            })}
-                        </Carousel>
-                    </Col>
-                </Row>
-            </Container>
+                <Container>
+                    <Row className="justify-content-center mt-3">
+                        <Col xs={6}>
+                            <h1>Welcome to Strivesturant !</h1>
+                            <p className="lead">The best page for all gratuated students!</p>
+                            <hr className=""></hr>
+                            <Carousel>
+                                {items.map((item) => {
+                                    return (
+                                        <Carousel.Item key={item.id}>
+                                            <img
+                                                className="d-block w-100"
+                                                src={item.image}
+                                                alt={item.name}
+                                                onClick={() => this.selectedDish(item)}
+                                            />
+                                            <Carousel.Caption>
+                                                <h3>{item.name}</h3>
+                                                <p>{item.description}</p>
+                                            </Carousel.Caption>
+                                        </Carousel.Item>
+                                    )
+                                })}
+                            </Carousel>
+                        </Col>
+                    </Row>
+                    <Dishcomments dish={this.state.dish} />
+                </Container>
             </>
         )
     }
