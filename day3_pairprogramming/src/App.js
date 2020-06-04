28be9f27cfd38bbbfc79c5ae45f804ebeb46d93c
klevinb <klevin.bazaiti98@gmail.com>
2020-06-04 15:40:17 +0200

    span messages added and BookList component

--- day3_pairprogramming/src/App.js
+++ day3_pairprogramming/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import AlertMessage from './components/WarningSign'
 import SingleBook from './components/SingleBook'
+import BookList from './components/BookList'
 import Badge from './components/MyBadge'
+import { Container, Row } from 'react-bootstrap'
 
 const books = {
   history: require('./data/history.json')
@@ -13,11 +15,18 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App" >
-        <AlertMessage text="This is an alert!" />
-        <Badge color="warning" text="KB" />
-        <SingleBook book={books.history} />
-      </div>
+      < div className="App" >
+        <Container className="mt-3">
+          <span><b>Aler Component</b></span>
+          <AlertMessage text="This is an alert!" />
+          <span><b>Badge Component</b></span>
+          <Badge color="warning" text="KB" />
+          <span><b>Single Book Component</b></span>
+          <SingleBook book={books.history.slice(0, 1)[0]} />
+          <span><b>BookList Component</b></span>
+          <BookList book={books.history.slice(0, 20)} />
+        </Container>
+      </div >
     );
   }
 }
