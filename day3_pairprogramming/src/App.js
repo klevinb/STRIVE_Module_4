759420dd22a9c50c9a5cf016ba54d31d223a6eb5
klevinb <klevin.bazaiti98@gmail.com>
2020-06-04 14:11:12 +0200

    SingleBook.jsx is called on App.jsx

--- day3_pairprogramming/src/App.js
+++ day3_pairprogramming/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import AlertMessage from './components/WarningSign'
+import SingleBook from './components/SingleBook'
 import Badge from './components/MyBadge'
 
-function App() {
-  return (
-    <div className="App">
-      <AlertMessage text="This is an alert!" />
-      <Badge color="warning" text="KB" />
-    </div>
-  );
+const books = {
+  history: require('./data/history.json')
+}
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App" >
+        <AlertMessage text="This is an alert!" />
+        <Badge color="warning" text="KB" />
+        <SingleBook book={books.history} />
+      </div>
+    );
+  }
 }
 
 export default App;
