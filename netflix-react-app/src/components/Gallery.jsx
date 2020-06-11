e6961acf0a7d5b352c39743182cf496c9584d95e
klevinb <klevin.bazaiti98@gmail.com>
2020-06-11 10:33:52 +0200

    Putting spiners until the data get fetched!

--- netflix-react-app/src/components/Gallery.jsx
+++ netflix-react-app/src/components/Gallery.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Row } from 'react-bootstrap'
 import Movie from './Movie'
+import { Col, Spinner } from 'react-bootstrap'
 
 function Gallery(props) {
     return (
         <div>
             <h4>{props.title}</h4>
             <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 no-gutters text-center">
-                <Movie src={props.imageSrc} />
+                {props.loading ?
+                    [1, 2, 3, 4, 5, 6].map((num) =>
+                        <Col>
+                            <Spinner animation="grow" variant="light" />
+                            <Spinner animation="grow" variant="light" size="sm" />
+                        </Col>
+                    )
+                    :
+                    props.movies.slice(0, 6).map((movie) =>
+                        <Movie src={movie} title={movie.Title} />
+                    )
+                }
             </Row>
         </div>
     );
