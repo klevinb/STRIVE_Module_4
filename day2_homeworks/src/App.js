f9bbb8287d4a357c864752ad4009f2393f56deb1
klevinb <klevin.bazaiti98@gmail.com>
2020-06-16 23:31:08 +0200

    Search users from an endpoint and display them!

--- day2_homeworks/src/App.js
+++ day2_homeworks/src/App.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container } from 'react-bootstrap'
+import { Container, ListGroup, Dropdown } from 'react-bootstrap'
 import Navbar from './components/MyNavbar'
 import Footer from './components/MyFooter'
 import Jumbotron from './components/Jumbotron'
-import Latestrelease from './components/LatestReleases';
 
 class App extends Component {
+
+
+  state = {
+    data: '',
+    search: null,
+    status: false
+  }
+
+  fetchData = async () => {
+    await fetch("https://striveschool.herokuapp.com/api/profile/", {
+      headers: new Headers({
+        'Authorization': 'Basic ' + "dXNlcjE2OmM5V0VVeE1TMjk0aE42ZkY=",
+        "Content-Type": "application/json",
+      }),
+    })
+      .then(response => response.json())
+      .then(respObj => this.setState({
+        data: respObj,
+      }))
+
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  searchedField = (searched) => {
+    if (searched) {
+      this.setState({
+        search: searched,
+        status: true
+      });
+    } else {
+      this.setState({
+        search: '',
+        status: false
+      });
+    }
+  }
+
 
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar search={this.searchedField} status={this.state.status} users={this.state.data && this.state.search &&
+          this.state.data.filter(user => user.name.toLowerCase().startsWith(this.state.search.toLowerCase())).length > 0 ?
+          this.state.data
+            .filter(user => user.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
+            .map((user, i) =>
+              <Dropdown.Item key={i} href="#/action-1">{user.name}</Dropdown.Item>
+            )
+          :
+          <Dropdown.Item href="#/action-1">No user</Dropdown.Item>
+        } />
         <Container>
           <Jumbotron />
-          <Latestrelease />
         </Container>
         <Footer />
       </div>
+
     );
   }
 }
--- day2_homeworks/src/components/MyNavbar.jsx
+++ day2_homeworks/src/components/MyNavbar.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav, FormControl, DropdownMenu, Dropdown } from 'react-bootstrap'
 import logo from '../assets/navbar_logo.png'
 
 class NavBar extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home"><img src={logo} width="120px" /></Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#browse">Browse</Nav.Link>
-                </Nav>
-            </Navbar>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home"><img src={logo} width="120px" /></Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#browse">Browse</Nav.Link>
+                    </Nav>
+                    <Dropdown show={this.props.status} className="mt-5">
+                        <Dropdown.Menu>
+                            {this.props.users}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <FormControl onChange={(e) => this.props.search(e.currentTarget.value)} style={{ width: 200 }} type="text" placeholder="Search" className="mr-sm-2" />
+
+                </Navbar>
             </>
         )
     }
